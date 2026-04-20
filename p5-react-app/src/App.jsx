57244455b071578d19dc5e7cde563a4832bdf3f1
Chester <atienzachester@gmail.com>
2026-04-20 23:09:58 +0800

    feat: base routing

--- p5-react-app/src/App.jsx
+++ p5-react-app/src/App.jsx
@@ -1,5 +1,19 @@
+import { Routes, Route } from "react-router-dom";
+
+import { Discovery } from "./pages/Discovery";
+import { Login } from "./pages/Login";
+import { Register } from "./pages/Register";
+import { Dashboard } from "./pages/Dashboard";
+
 function App() {
-  return <h1>Auto Hub</h1>;
+  return (
+    <Routes>
+      <Route path="/" element={<Discovery />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+    </Routes>
+  );
 }
 
 export default App;
